6197dae87d4526b5c75bc1183bac0746eba1073f
tuananhroman <anh.maxiking@hotmail.de>
2021-01-13 23:47:40 +0100

    minor changes

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/tools/train_agent_utils.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/tools/train_agent_utils.py
@@ -2,11 +2,11 @@ import os
 import datetime
 import json
 
-def write_hyperparameters_json(agent_hyperparams: object, PATHS: dict):
+def write_hyperparameters_json(hyperparams: object, PATHS: dict):
     doc_location = os.path.join(PATHS.get('model'), "hyperparameters.json")
 
     with open(doc_location, "w", encoding='utf-8') as target:
-        json.dump(agent_hyperparams.__dict__, target, ensure_ascii=False, indent=4)
+        json.dump(hyperparams.__dict__, target, ensure_ascii=False, indent=4)
 
 
 def update_total_timesteps_json(timesteps: int, PATHS:dict):
@@ -27,12 +27,15 @@ def update_total_timesteps_json(timesteps: int, PATHS:dict):
         print("Found no 'hyperparameters.json' in model directory (%s) !" % PATHS.get('model'))
 
 
-def print_hyperparameters_json(PATHS:dict):
+def print_hyperparameters_json(hyperparams_obj: object, PATHS:dict):
     doc_location = os.path.join(PATHS.get('model'), "hyperparameters.json")
 
     if os.path.isfile(doc_location):
         with open(doc_location, "r") as file:
             hyperparams = json.load(file)
+
+        if not check_hyperparam_format(hyperparams_obj, hyperparams):
+            raise AssertionError("'hyperparameters.json' in %s has wrong format!" % PATHS.get('model'))
 
         print("\n--------------------------------")
         print("         HYPERPARAMETERS         \n")
@@ -41,3 +44,9 @@ def print_hyperparameters_json(PATHS:dict):
         print("--------------------------------\n\n")
     else:
         print("Warning: found no 'hyperparameter.json' in %s" % PATHS.get('model'))
+
+
+def check_hyperparam_format(hyperparams_obj: object, loaded_hyperparams: dict):
+    if set(hyperparams_obj.__dict__.keys()) == set(loaded_hyperparams.keys()):
+        return True
+    return False
