7087f3ad2d7c6cfa51ba61022a99467f8c3a404f
linhdoan8 <linh4138@yahoo.de>
2021-01-19 00:25:21 +0100

    corrected bugs in gerneate empyt map, small changes in sim params

--- task_generator/task_generator/tasks.py
+++ task_generator/task_generator/tasks.py
@@ -146,7 +146,7 @@ def get_predefined_task(mode="random"):
     # obstacles_manager.register_obstacles(3, os.path.join(
     # models_folder_path, "obstacles", 'random.model.yaml'), 'static')
     # generate 5 static or dynamic obstaticles
-    obstacles_manager.register_random_obstacles(20, 0.4)
+    obstacles_manager.register_random_obstacles(15, 0.4)
 
     # TODO In the future more Task will be supported and the code unrelated to
     # Tasks will be moved to other classes or functions.
