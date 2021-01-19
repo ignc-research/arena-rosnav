6eab1d687f78fcbd48d43430c0c18ccd248a48d0
tuananhroman <anh.maxiking@hotmail.de>
2021-01-19 14:53:55 +0100

    will read json to get current stage when resuming training

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     env = DummyVecEnv([lambda: FlatlandEnv(task_manager, PATHS.get('robot_setting'), PATHS.get('robot_as'), params['discrete_action_space'], goal_radius=1.00, max_steps_per_episode=350)] * n_envs)
     
     # instantiate eval environment
-    trainstage_cb = InitiateNewTrainStage(TaskManager=task_manager, TreshholdType="rew", rew_threshold=13.5, verbose=1)
+    trainstage_cb = InitiateNewTrainStage(TaskManager=task_manager, TreshholdType="rew", rew_threshold=13.5, StartAt=params['curr_stage'], verbose=1)
     eval_env = Monitor(FlatlandEnv(task_manager, PATHS.get('robot_setting'), PATHS.get('robot_as'), params['discrete_action_space'], goal_radius=1.00, max_steps_per_episode=350), PATHS.get('eval'), info_keywords=("done_reason",))
     eval_cb = EvalCallback(eval_env, n_eval_episodes=20, eval_freq=10000, log_path=PATHS.get('eval'), best_model_save_path=PATHS.get('model'), deterministic=True, callback_on_new_best=trainstage_cb)
 
