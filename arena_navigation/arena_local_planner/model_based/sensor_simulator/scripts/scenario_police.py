7edc9f36780d0d39ac95458e290057b1129e4cd5
Teham Bhuiyan <teham@live.de>
2021-01-30 18:31:45 +0100

    ready for eval

--- arena_navigation/arena_local_planner/model_based/sensor_simulator/scripts/scenario_police.py
+++ arena_navigation/arena_local_planner/model_based/sensor_simulator/scripts/scenario_police.py
@@ -4,14 +4,40 @@ import math
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Int16
+from visualization_msgs.msg import Marker
+from nav_msgs.msg import Path
 # 
 class police():
     def __init__(self):
         self.n_col = 0
+        self.n_replan_pm = 0
+        self.n_replan_mb = 0
         self.collision_flag = False
+        # sub
         self.scan = rospy.Subscriber('/scan',LaserScan, self.cbScan)
-        self.pub_col = rospy.Publisher('/collision', Int16, queue_size=10)
+        rospy.Subscriber('/planning_vis/goal',Marker, self.get_pm_path)
+        rospy.Subscriber('/move_base/DWAPlannerROS/global_plan',Path, self.get_mb_path)
+        rospy.Subscriber('/move_base/TebLocalPlannerROS/global_plan',Path, self.get_mb_path)
+        rospy.Subscriber('/move_base/MpcLocalPlannerROS/global_plan',Path, self.get_mb_path)
 
+        # pub
+        self.pub_col = rospy.Publisher('police/collision', Int16, queue_size=10)
+        self.pub_mb_replan = rospy.Publisher('police/mb_replanned', Int16, queue_size=10)
+        self.pub_pb_replan = rospy.Publisher('police/pm_replanned', Int16, queue_size=10)
+
+
+        rospy.Timer(rospy.Duration(0.5),self.publish_replans)
+
+    def get_pm_path(self,msg):
+        self.n_replan_pm += 1
+
+    def get_mb_path(self,msg):
+        self.n_replan_mb += 1
+
+
+    def publish_replans(self, event):
+        self.pub_mb_replan.publish(self.n_replan_mb)
+        self.pub_pb_replan.publish(self.n_replan_pm)
 
     def cbScan(self,msg):
         scan_array = np.asarray(msg.ranges)
