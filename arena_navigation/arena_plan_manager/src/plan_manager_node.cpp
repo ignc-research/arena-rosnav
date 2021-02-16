e12aea6d1cd801e70c342f0ec1ee70c56f1f8328
tuananhroman <anh.maxiking@hotmail.de>
2021-02-16 23:30:16 +0100

    subgoal bug fixed, reverted changes for global path, everything ok now

--- arena_navigation/arena_plan_manager/src/plan_manager_node.cpp
+++ arena_navigation/arena_plan_manager/src/plan_manager_node.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "plan_manager");
     std::string ns = ros::this_node::getNamespace();
     //ros::NodeHandle node_handle("~"); every topic will be with namespace
-    ros::NodeHandle node_handle;
+    ros::NodeHandle node_handle("");
     //ros::WallRate r(100);
     PlanManager plan_manager;
     plan_manager.init(node_handle);
