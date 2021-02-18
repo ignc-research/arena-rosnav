1de7142437cdbe4fb2fd4f563c0d802838f909e7
Yi Sun <sun@instance-1.us-central1-a.c.commanding-hawk-300819.internal>
2021-02-18 17:00:06 +0000

    add docker for training

---
+++ Docker/entrypoint.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# In the ~/.bashrc
+export WORKON_HOME=/root/.python_env
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+source /opt/ros/melodic/setup.bash
+source /usr/local/bin/virtualenvwrapper.sh
+source /root/catkin_ws/devel/setup.bash
+export PYTHONPATH=//geometry2_ws/devel/lib/python3/dist-packages:${PYTHONPATH}
+export PYTHONPATH=/root/catkin_ws/src/arena-rosnav/:${PYTHONPATH}
+
+JQ_EXEC=`which jq`
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
+AGENT_NAME=$1
+NUM_SIM_ENVS$2
+CONFIG_FILE=$3
+# CONFIG_FILE="config1.json"
+
+# AGENT_NAME=$(cat $SHELL_FOLDER/configs/${CONFIG_FILE} | ${JQ_EXEC} .input.AGENT_NAME | sed 's/\"//g')
+# NUM_SIM_ENVS=$(cat $SHELL_FOLDER/configs/${CONFIG_FILE} | ${JQ_EXEC} .input.NUM_SIM_ENVS)
+
+# robot=$(cat $SHELL_FOLDER/configs/${CONFIG_FILE} | ${JQ_EXEC} .parameters.robot)
+# gamma=$(cat $SHELL_FOLDER/configs/${CONFIG_FILE} | ${JQ_EXEC} .parameters.gamma)
+
+echo "========================="
+echo "AGENT_NAME is : $AGENT_NAME"
+echo "NUM_SIM_ENVS is : $NUM_SIM_ENVS"
+# echo "========================="
+# echo "robot is : $robot"
+# echo "gamma is : $gamma"
+
+
+(
+    source "/root/.python_env/rosnav/bin/activate"
+    roslaunch arena_bringup start_training.launch num_envs:=${NUM_SIM_ENVS}
+)&
+(
+    roslaunch arena_bringup visualization_training.launch ns:=sim_1
+)&
+(
+    sleep 2
+    source "/root/.python_env/rosnav/bin/activate"
+    roscd arena_local_planner_drl
+    python scripts/training/train_agent.py --agent ${AGENT_NAME} --config $CONFIG_FILE
+    # python scripts/training/train_agent.py --agent 
+)
+
+# -v configs
