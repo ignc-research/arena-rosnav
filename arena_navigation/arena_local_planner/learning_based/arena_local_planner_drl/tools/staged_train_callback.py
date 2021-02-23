05ed70dc18771c6c7b7c5cac35c17a7220daf006
tuananhroman <anh.maxiking@hotmail.de>
2021-02-23 12:48:25 +0100

    fixed stoptrainatthreshold

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/tools/staged_train_callback.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/tools/staged_train_callback.py
@@ -64,7 +64,8 @@ class InitiateNewTrainStage(BaseCallback):
                     
             if ((self.threshhold_type == "rew" and EvalObject.best_mean_reward >= self.upper_threshold) or
                 (self.threshhold_type == "succ" and EvalObject.last_success_rate >= self.upper_threshold)):
-                for task_manager in self.TaskManagers:
-                    task_manager.next_stage()
-                EvalObject.best_mean_reward = -np.inf
-                EvalObject.last_success_rate = -np.inf
+                if self.task_manager[0]._curr_stage != self.task_manager[0].get_num_stages():
+                    for task_manager in self.TaskManagers:
+                        task_manager.next_stage()
+                    EvalObject.best_mean_reward = -np.inf
+                    EvalObject.last_success_rate = -np.inf
