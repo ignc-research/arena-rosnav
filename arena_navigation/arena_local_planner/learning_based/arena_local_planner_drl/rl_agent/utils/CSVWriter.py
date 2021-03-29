2a2d5c776299ed0510b6410c8a3d1fc2cafe8ec4
Sirupli <junhuili9408@gmail.com>
2021-03-29 11:48:31 +0200

    add CSV writer

---
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/utils/CSVWriter.py
@@ -0,0 +1,17 @@
+import csv
+import numpy as np
+# 'w' = write mode
+class CSVWriter():
+    def __init__(self):
+        # create evaluation.csv file
+        with open('evaluation.csv', 'w+', newline = '') as fp:
+            a = csv.writer(fp, delimiter = ',')
+            data = [['episode', 'nearest distance for adult','nearest distance for child','nearest distance for elder','reward','done_reason']]
+            a.writerows(data)
+
+    def addData(self, data:np.array):
+        #add new data to the file 
+        with open('evaluation.csv', 'a+', newline = '') as fp:
+            a = csv.writer(fp, delimiter = ',')
+            data =data.reshape(1,-1)
+            a.writerows(data)
