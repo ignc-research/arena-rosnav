bfcd46de1cf301ec43077662900c4f2bc9db71ac
Timo <timo@DESKTOP-MQT7SVA.localdomain>
2021-03-31 13:12:20 +0200

    first changes on wp3_env

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/envs/new.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/envs/new.py
@@ -10,7 +10,7 @@ import yaml
 from arena_navigation.arena_local_planner.learning_based.arena_local_planner_drl.rl_agent.utils.observation_collector import ObservationCollector
 from arena_navigation.arena_local_planner.learning_based.arena_local_planner_drl.rl_agent.utils.reward import RewardCalculator
 from arena_navigation.arena_local_planner.learning_based.arena_local_planner_drl.rl_agent.utils.debug import timeit
-from task_generator.tasks import ABSTask 
+from task_generator.tasks import ABSTask
 import numpy as np
 import rospy
 from geometry_msgs.msg import Twist, PoseStamped, Pose2D
@@ -23,10 +23,10 @@ import subprocess
 
 from arena_plan_msgs.msg import RobotState, RobotStateStamped
 
-class wp3Env(gym.Env):
+class wpEnv(gym.Env):
     """Custom Environment that follows gym interface"""
 
-    def __init__(self, task: ABSTask, robot_yaml_path: str, settings_yaml_path: str, is_action_space_discrete, safe_dist: float = None, goal_radius: float = 0.1, max_steps_per_episode=100):
+    def __init__(self, ns: str, task: ABSTask, robot_yaml_path: str, settings_yaml_path: str, reward_fnc: str, is_action_space_discrete, safe_dist: float = None, goal_radius: float = 0.1, max_steps_per_episode=100, train_mode: bool = True, debug: bool = False):
         """Default env
         Flatland yaml node check the entries in the yaml file, therefore other robot related parameters cound only be saved in an other file.
         TODO : write an uniform yaml paser node to handel with multiple yaml files.
@@ -37,19 +37,33 @@ class wp3Env(gym.Env):
             task (ABSTask): [description]
             robot_yaml_path (str): [description]
             setting_yaml_path ([type]): [description]
+            reward_fnc (str): [description]
             is_action_space_discrete (bool): [description]
             safe_dist (float, optional): [description]. Defaults to None.
             goal_radius (float, optional): [description]. Defaults to 0.1.
         """
-        super(wp3Env, self).__init__()
-       
+        super(wpEnv, self).__init__()
+
+        self.ns = ns
+        
+        # process specific namespace in ros system
+        if ns is not None or ns !="":
+            self.ns_prefix = '/'+ns + '/'
+        else:
+            self.ns_prefix = '/'
+        
+        if not debug:
+            if train_mode:
+                rospy.init_node(f'traintrain_env_{self.ns}', disable_signals=False)
+            else:
+                rospy.init_node(f'evaleval_env_{self.ns}', disable_signals=False)
         # Define action and observation space
         # They must be gym.spaces objects
         self._is_action_space_discrete = is_action_space_discrete
         self.setup_by_configuration(robot_yaml_path, settings_yaml_path)
         # observation collector
         self.observation_collector = ObservationCollector(
-            self._laser_num_beams, self._laser_max_range)
+            self.ns, self._laser_num_beams, self._laser_max_range)
         self.observation_space = self.observation_collector.get_observation_space()
 
         # reward calculator
@@ -57,37 +71,34 @@ class wp3Env(gym.Env):
             safe_dist = 1.6*self._robot_radius
 
         self.reward_calculator = RewardCalculator(
-             robot_radius=self._robot_radius, safe_dist=1.2*self._robot_radius, goal_radius=goal_radius)
+            robot_radius=self._robot_radius, safe_dist=1.2*self._robot_radius, goal_radius=goal_radius, rule=reward_fnc)
         #subscriber to infer callback and out of sleep loop
         #sub robot position and sub global goal 
-        self._robot_state_sub = rospy.Subscriber('/odom', Odometry, self.cbRobotPosition)            # for robot orientation
-        self._ref_wp_sub = rospy.Subscriber('/plan_manager/subgoal', PoseStamped, self.cbRefWp)                   # for subgoal position (reference point for circle)
-        self._globalGoal = rospy.Subscriber('/goal', PoseStamped, self.cbGlobalGoal)                 # to set wp on global goal if circle touces it
-        #TODO: check if global plan is still needed or can be deleted
-        self._globalPlan_sub = rospy.Subscriber('/plan_manager/globalPlan', Path, self.cbglobalPlan)
-        self._twist_sub = rospy.Subscriber('/cmd_vel', Twist, self.cbTwist)                          # to calculate distance traveled 
+        self._robot_state_sub = rospy.Subscriber(f'{self.ns_prefix}odom', Odometry, self.cbRobotPosition)            # for robot orientation
+        self._ref_wp_sub = rospy.Subscriber(f'{self.ns_prefix}subgoal', PoseStamped, self.cbRefWp)                   # for subgoal position (reference point for circle)
+        self._globalGoal = rospy.Subscriber(f'{self.ns_prefix}goal', PoseStamped, self.cbGlobalGoal)                 # to set wp on global goal if circle touces it
+        self._twist_sub = rospy.Subscriber(f'{self.ns_prefix}cmd_vel', Twist, self.cbTwist)                          # to calculate distance traveled 
         self._wp4train_reached =False
-
+        
         # action agent publisher
-        self.agent_action_pub = rospy.Publisher('/plan_manager/wp4train', PoseStamped, queue_size=1)        # agent publishes a waypoint on a circle 
-        self.circle_pub = rospy.Publisher('/zViz', Path, queue_size=1)                               # create the circle on which the wp are placed for visualization
+        self.agent_action_pub = rospy.Publisher(f'{self.ns_prefix}wp_drl/wp4train', PoseStamped, queue_size=1)        # agent publishes a waypoint on a circle 
+        self.circle_pub = rospy.Publisher(f'{self.ns_prefix}zViz', Path, queue_size=1)                               # create the circle on which the wp are placed for visualization
 
 
         # service clients
-        self._is_train_mode = rospy.get_param("train_mode")
+        self._is_train_mode = rospy.get_param("/train_mode")
         if self._is_train_mode:
-            self._service_name_step = '/step_world'
+            self._service_name_step = f'{self.ns_prefix}step_world'
             self._sim_step_client = rospy.ServiceProxy(
             self._service_name_step, StepWorld)
         self.task = task
-
+        
         self._max_steps_per_episode = max_steps_per_episode
         #global variables for subscriber callbacks
         self.range_circle = 1.5
         self._steps_curr_episode = 0
         self._robot_pose = PoseStamped()
-        #TODO: check if global plan still need it and delete if not
-        self._globalPlan = Path()
+
         self._subgoal = Pose2D()
         self._globalGoal = Pose2D()
         self._ref_wp = PoseStamped()
@@ -109,7 +120,7 @@ class wp3Env(gym.Env):
         self._robot_pose.pose.position.x = msg.pose.pose.position.x
         self._robot_pose.pose.position.y = msg.pose.pose.position.y
         self._robot_pose.pose.orientation = msg.pose.pose.orientation
- 
+
         
     def cbTwist(self,msg):
         self._robot_twist[0]= msg.linear.x
@@ -160,7 +171,7 @@ class wp3Env(gym.Env):
                 self.action_space = spaces.Discrete(
                     len(self._discrete_acitons))
             else:
-
+                
                 angular_range = setting_data['robot']['continuous_actions']['angular_range']
                 self.action_space = spaces.Box(low=np.array([angular_range[0]]),
                                                high=np.array([angular_range[1]]), dtype=np.float)
@@ -173,27 +184,26 @@ class wp3Env(gym.Env):
         return output, error
 
     def _calc_distance(self, goal_pos:Pose2D,robot_pos:Pose2D):
-         y_relative = goal_pos.y - robot_pos.y
-         x_relative = goal_pos.x - robot_pos.x
-         rho =  (x_relative**2+y_relative**2)**0.5
-         theta = (np.arctan2(y_relative,x_relative)-robot_pos.theta+4*np.pi)%(2*np.pi)-np.pi
-         return rho,theta
+        y_relative = goal_pos.y - robot_pos.y
+        x_relative = goal_pos.x - robot_pos.x
+        rho =  (x_relative**2+y_relative**2)**0.5
+        theta = (np.arctan2(y_relative,x_relative)-robot_pos.theta+4*np.pi)%(2*np.pi)-np.pi
+        return rho,theta
 
     def _pub_action(self, action):
 
         _, obs_dict = self.observation_collector.get_observations()
         dist_robot_goal = obs_dict['goal_in_robot_frame']
-        #TODO: check if still needed and delete if not
-        dist_global_sub = obs_dict['global_in_subgoal_frame']
+
         #transform action which is a waypoint to 2d to calculate distance robot-wp
-        wp2d = Pose2D()
+        wp2d = Pose2D()     
         wp2d.x = self._action_msg.pose.position.x
         wp2d.y = self._action_msg.pose.position.y
 
         robot_pos =  Pose2D()
         robot_pos.x = self._robot_pose.pose.position.x 
         robot_pos.y = self._robot_pose.pose.position.y
-    
+
         #calculate distance between robot and waypoint
         dist_robot_wp = self._calc_distance(wp2d, robot_pos)
         self._action_msg.pose.orientation.z =  0 
@@ -211,7 +221,7 @@ class wp3Env(gym.Env):
         while (i < 2):
             point = PoseStamped()
             q = self._robot_pose.pose.orientation
-            robot_angle = np.arctan2(2.0*(q.w*q.z + q.x*q.y), 1-2*(q.y*q.y+q.z*q.z))
+            robot_angle = np.arctan2(2.0*(q.w*q.z + q.x*q.y), 1-2*(q.y*q.y+q.z*q.z)) 
             angle_grad = i + robot_angle 
             point.pose.position.x = self._ref_wp.pose.position.x + (self.range_circle*math.cos(angle_grad))         
             point.pose.position.y = self._ref_wp.pose.position.y + (self.range_circle*math.sin(angle_grad))
@@ -220,7 +230,7 @@ class wp3Env(gym.Env):
             circle.poses.append(point)
 
             # if circle touches the global goal, immediately place subgoal on global goal
-            if ((math.isclose(circle.poses[j].pose.position.x, self._globalGoal.x, rel_tol=0.2)) and (math.isclose(circle.poses[j].pose.position.y, self._globalGoal.y, rel_tol=0.2))):    
+            if ((math.isclose(circle.poses[j].pose.position.x, self._globalGoal.x, rel_tol=0.2)) and (math.isclose(circle.poses[j].pose.position.y, self._globalGoal.y, rel_tol=0.2))):       
                 self._action_msg.pose.position.x = self._globalGoal.x 
                 self._action_msg.pose.position.y = self._globalGoal.y 
                 self._action_msg.pose.orientation.w = 1
@@ -232,7 +242,7 @@ class wp3Env(gym.Env):
         #visualize circle by publishing all poses to /zViz topic
         self.circle_pub.publish(circle)
        
-       # First spawn 
+        # First spawn 
         if self.firstTime < 1:
             q = self._robot_pose.pose.orientation
             robot_angle = np.arctan2(2.0*(q.w*q.z + q.x*q.y), 1-2*(q.y*q.y+q.z*q.z))
@@ -243,7 +253,7 @@ class wp3Env(gym.Env):
             self.firstTime +=1
             self._action_count += 1
             print("distance robot to wp: {}".format(dist_robot_wp[0]))
-            
+
         #wait for robot to reach the waypoint first
         #if self._step_counter - self._previous_time > 30:
         if dist_robot_wp[0] < 0.6:
@@ -251,9 +261,6 @@ class wp3Env(gym.Env):
             _, obs_dict = self.observation_collector.get_observations()
             dist_robot_goal = obs_dict['goal_in_robot_frame']
             
-            #TODO check if still needed and delete if not
-            #dist_robot_goal = np.array([self._robot_pose.x - self._subgoal.x, self._robot_pose.y - self._subgoal.y])
-            dist_rg = np.linalg.norm(dist_robot_goal)            
             #todo consider the distance to global path when choosing next optimal waypoint
             #send a goal message (posestamped) as action, remeber to normalize the quaternions (put orientationw as 1) and set the frame id of the goal to "map"! 
             if dist_robot_goal[0] < 2:
@@ -291,25 +298,17 @@ class wp3Env(gym.Env):
         new_action = [0]*2
         new_action[0] = self._robot_twist[0]
         new_action[1] = self._robot_twist[1]
-        
+       
         # wait for new observations
         s = time.time()
         merged_obs, obs_dict = self.observation_collector.get_observations()
         # print("get observation: {}".format(time.time()-s))
 
-        # get global path once new episode started and round to integer for reward waypoints_set relative to goal distance
-        if self.firstTime < 2:
-            self.goal_len = int(round(obs_dict['goal_in_robot_frame'][0]))
-        # provide reward calculator with the data to calculate reward
+        # calculate reward
         reward, reward_info = self.reward_calculator.get_reward(
-            obs_dict['laser_scan'], obs_dict['goal_in_robot_frame'],
-            robot_pose=obs_dict['robot_pose'], 
-            globalPlan=self._globalPlan, 
-            action=new_action, 
-            goal_len=self.goal_len, 
-            action_count= self._action_count,
-            subgoal=self._action_msg.pose.position)
-
+            obs_dict['laser_scan'], obs_dict['goal_in_robot_frame'], 
+            action=new_action, global_plan=obs_dict['global_plan'], robot_pose=obs_dict['robot_pose'])
+        # print(f"cum_reward: {reward}")
         done = reward_info['is_done']
         print("reward:  {}".format(reward))
         # info
@@ -328,8 +327,7 @@ class wp3Env(gym.Env):
         return merged_obs, reward, done, info
 
     def reset(self):
-        #TODO check if still needed and comment if not
-        self.clear_costmaps()
+        #self.clear_costmaps()
 
         # set task
         # regenerate start position end goal position of the robot and change the obstacles accordingly
@@ -350,21 +348,21 @@ class wp3Env(gym.Env):
 
 if __name__ == '__main__':
 
-    rospy.init_node('wp3_gym_env', anonymous=True)
+    rospy.init_node('wp_gym_env', anonymous=True, disable_signals=False)
     print("start")
 
-    wp3_env = wp3Env()
-    check_env(wp3_env, warn=True)
+    wp_env = wpEnv()
+    check_env(wp_env, warn=True)
 
     # init env
-    obs = wp3_env.reset()
+    obs = wp_env.reset()
 
     # run model
     n_steps = 200
     for step in range(n_steps):
         # action, _states = model.predict(obs)
-        action = wp3_env.action_space.sample()
+        action= wp_env.action_space.sample()
 
-        obs, rewards, done, info = wp3_env.step(action)
+        obs, rewards, done, info = wp_env.step(action)
 
         time.sleep(0.1)
