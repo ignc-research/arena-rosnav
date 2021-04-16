776438ca294d89fe8a7f41322b6ad9c5bd94db33
Michael Stenzel <michael.e.stenzel@gmail.com>
2021-04-16 03:09:57 +0200

    Swapped for loop for while loop, recording is now done for the number of repeats specified in the scenario json

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_il/rollouts/record_rollouts.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_il/rollouts/record_rollouts.py
@@ -24,23 +24,25 @@ models_folder_path = rospkg.RosPack().get_path('simulator_setup')
 arena_local_planner_drl_folder_path = rospkg.RosPack().get_path(
     'arena_local_planner_drl')
 
-
 env = FlatlandEnv(task, os.path.join(models_folder_path, 'robot', 'myrobot.model.yaml'),
                   os.path.join(arena_local_planner_drl_folder_path,
                                'configs', 'default_settings.yaml'), "rule_00", False,
                   )
 
 obs = env.reset()
-#TODO end recording loop with keypress or by ending when the scenario has been repeated the correct number of times
 observations = []
 actions = []
-for i in range(20000):
-    print(i)
+while(True):
     merged_obs, obs_dict, action = env.observation_collector.get_observations_and_action()
     observations.append(merged_obs)
     actions.append(action)
+    #TODO improvement: get info from check_if_done; if the robot collided, don't save (obs, action) to the arrays
     if env.check_if_done(merged_obs, obs_dict):
-        env.reset()
+        try:
+            env.reset()
+        except:
+            print('All scenarios have been evaluated!')
+            break
 
 # save rollouts
 if args.outputformat == 'h5':
@@ -65,35 +67,10 @@ if args.outputformat == 'npz':
     # save observations and actions in an npz file:
     date_str = datetime.now().strftime('%Y%m%d_%H-%M')
     path = str(f'./output/rollout_{date_str}')
+    print(f'Saving rollouts to {path}')
     np.savez_compressed(
         path,
         observations = np.array(observations),
         actions = np.array(actions)
     )
-
-# How should the obstacles appear?
-#TODO need to reset task (either directly through task.reset() or indirectly through Flatlandenv.reset() to return robot and obstacles
-# to starting position)
-#TODO need to add check that goal has been reached e.g. by overriding flatlandenv's step function or (simpler) by 
-# adding a function here that extracts goal in robot frame from obs and checks that it is within a tolerance range
-#TODO could also use get_reward to check for collisions or cases where the robot gets stuck
-
-#use env.reset()
-#and 
-# calculate reward
-#reward, reward_info = self.reward_calculator.get_reward(
-#    obs_dict['laser_scan'], obs_dict['goal_in_robot_frame'])
-#done = reward_info['is_done']
-#
-#print("reward:  {}".format(reward))
-#
-## info
-#info = {}
-#if done:
-#   info['done_reason'] = reward_info['done_reason']
-#else:
-#if self._steps_curr_episode == self._max_steps_per_episode:
-#   done = True
-#   info['done_reason'] = 0
-
-#obs = env.reset()
+    print('Done saving rollouts')
