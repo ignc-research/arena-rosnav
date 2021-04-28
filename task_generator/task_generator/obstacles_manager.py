5371ad5aba373e8cc36a8236decd8a479ea5dd43
basselfa <ba.f.prosoft@gmail.com>
2021-04-28 15:28:25 +0200

    corrected ids

--- task_generator/task_generator/obstacles_manager.py
+++ task_generator/task_generator/obstacles_manager.py
@@ -688,7 +688,8 @@ class ObstaclesManager:
         self.agent_topic_str=''   
      
         for ped in peds:            
-            elements = [0, 1, 3, 4]
+            elements = [0, 1, 2, 3]
+          
             # probabilities = [0.4, 0.3, 0.3] np.random.choice(elements, 1, p=probabilities)[0]
             self.__ped_type=elements[(ped[0]-1)%4]
             if  self.__ped_type==0:
@@ -699,11 +700,11 @@ class ObstaclesManager:
                 self.agent_topic_str+=f',{self.ns_prefix}pedsim_agent_{ped[0]}/dynamic_child'
                 self.__ped_file=os.path.join(rospkg.RosPack().get_path(
                 'simulator_setup'), 'dynamic_obstacles/person_two_legged_child.model.yaml')
-            elif self.__ped_type==3:
+            elif self.__ped_type==2:
                 self.agent_topic_str+=f',{self.ns_prefix}pedsim_agent_{ped[0]}/dynamic_elder'
                 self.__ped_file=os.path.join(rospkg.RosPack().get_path(
                 'simulator_setup'), 'dynamic_obstacles/person_single_circle_elder.model.yaml')
-            elif self.__ped_type==4:
+            elif self.__ped_type==3:
                 self.agent_topic_str+=f',{self.ns_prefix}pedsim_agent_{ped[0]}/dynamic_forklift     '
                 self.__ped_file=os.path.join(rospkg.RosPack().get_path(
                 'simulator_setup'), 'dynamic_obstacles/big_forklift.model.yaml')
@@ -730,6 +731,7 @@ class ObstaclesManager:
                 p.z = pos[2]
                 msg.waypoints.append(p)
             srv.peds.append(msg)
+            print(ped[0], msg.type)
 
         max_num_try = 2
         i_curr_try = 0
