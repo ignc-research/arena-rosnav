5e50344612e457552e09b356ed15d2c2798a58ed
tuananhroman <anh.maxiking@hotmail.de>
2021-05-05 15:32:05 +0200

    minor fix

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
     
     # for training with start_arena_flatland.launch
     ros_params = rospy.get_param_names()
-    ns_for_nodes = False if 'single_env' in ros_params else True
+    ns_for_nodes = False if '/single_env' in ros_params else True
 
     # check if simulations are booted
     wait_for_nodes(with_ns=ns_for_nodes, n_envs=args.n_envs, timeout=5)
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
     # instantiate train environment
     # when debug run on one process only
-    if not args.debug and ns_for_nodes:
+    if not args.debug and not ns_for_nodes:
         env = SubprocVecEnv(
             [make_envs(ns_for_nodes, i, params=params, PATHS=PATHS) 
                 for i in range(args.n_envs)], 
