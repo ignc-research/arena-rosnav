2ceaaafca358a794b7419ae5e46bd91c71c9237d
dangaw255 <danielgawrischgithub@gmail.com>
2021-05-11 13:23:50 +0200

    edit pedsim test

--- utils/scripts/arena_util_scripts/scripts/pedsim_test.py
+++ utils/scripts/arena_util_scripts/scripts/pedsim_test.py
@@ -17,44 +17,38 @@ def example1():
 
     peds = []
 
-    ped1 = Ped()
-    ped1.id = 2
-    ped1.pos = Point(6, 3, 0)
-    ped1.type = 0
-    ped1.number_of_peds = 5
-    ped1.vmax = 1.0
-    ped1.force_factor_desired = 1.0
-    ped1.force_factor_obstacle = 1.0
-    ped1.force_factor_social = 1.0
-    ped1.waypoint_mode = 1
-    ped1.waypoints = [Point(7, 2, 0.1), Point(7, 8, 0.1), Point(3, 9, 0.1)]
-    ped1.yaml_file = os.path.join(rospack.get_path("simulator_setup"), "dynamic_obstacles", "person_two_legged.model.yaml")
-    peds.append(ped1)
+    # ped1 = Ped()
+    # ped1.id = 2
+    # ped1.pos = Point(6, 3, 0)
+    # ped1.type = 0
+    # ped1.number_of_peds = 8
+    # ped1.max_talking_distance = 3
+    # ped1.chatting_probability = 0.1
+    # ped1.vmax = 2.0
+    # ped1.force_factor_desired = 5.0
+    # ped1.force_factor_obstacle = 1.0
+    # ped1.force_factor_social = 1.0
+    # ped1.waypoint_mode = 1
+    # ped1.waypoints = [Point(7, 2, 0.1), Point(7, 8, 0.1)]
+    # ped1.yaml_file = os.path.join(rospack.get_path("simulator_setup"), "dynamic_obstacles", "person_two_legged.model.yaml")
+    # peds.append(ped1)
 
     ped2 = Ped()
     ped2.id = 2
-    ped2.pos = Point(1, 3, 0)
-    ped2.type = 0
-    ped2.number_of_peds = 3
-    ped2.vmax = 1.0
+    ped2.pos = Point(2, 3, 0)
+    ped2.type = 3
+    ped2.number_of_peds = 1
+    ped2.vmax = 2.0
     ped2.force_factor_desired = 1.0
     ped2.force_factor_obstacle = 1.0
     ped2.force_factor_social = 1.0
     ped2.waypoint_mode = 1
-    ped2.waypoints = [Point(3, 3, 0.1), Point(3, 8, 0.1), Point(8, 8, 0.1), Point(8, 3, 0.1)]
-    ped2.yaml_file = os.path.join(rospack.get_path("simulator_setup"), "dynamic_obstacles", "person_two_legged.model.yaml")
+    ped2.waypoints = [Point(10, 3, 0.1), Point(3, 8, 0.1), Point(8, 8, 0.1), Point(3, 3, 0.1)]
+    ped2.yaml_file = os.path.join(rospack.get_path("simulator_setup"), "dynamic_obstacles", "big_forklift.model.yaml")
     peds.append(ped2)
 
     response = spawn_ped_srv.call(peds)
     print("successfully spawned peds" if response.success else "failed")
-
-    time.sleep(5)
-
-    remove_all_peds_service_name = "pedsim_simulator/remove_all_peds"
-    rospy.wait_for_service(remove_all_peds_service_name, 6.0)
-    remove_all_peds_srv = rospy.ServiceProxy(remove_all_peds_service_name, SetBool)
-    response = remove_all_peds_srv.call()
-    print("remove all peds: ", response.success)
 
 
 def example2():
@@ -133,4 +127,4 @@ def example2():
 
 
 if __name__ == '__main__':
-    example2()
+    example1()
