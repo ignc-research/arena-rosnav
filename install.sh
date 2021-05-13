123af8eb6015f3a71ce02d5129b94bd931b23191
wittenator <wittenator>
2021-05-13 11:57:29 +0200

    Moved python packages from system to poetry env

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-mkdir -p catkin_ws/src && cd catkin_ws/src
+mkdir -p catkin_ws/src && 
+cd catkin_ws/src
 git clone git@github.com:wittenator/arena-rosnav.git
 cd arena-rosnav
 
@@ -9,17 +10,16 @@ sudo apt update
 
 sudo apt-get install aptitude
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu bionic main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo aptitude update
-sudo aptitude -y install ros-melodic-desktop-full
+sudo aptitude -y install ros-noetic-desktop-full
 
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo aptitude -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
-
+sudo aptitude -y install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 sudo rosdep init
 rosdep update
 
@@ -27,24 +27,22 @@ sudo aptitude update && sudo aptitude -y install \
 libopencv-dev \
 liblua5.2-dev \
 screen \
-python3-catkin-pkg-modules \
 python3-rospkg-modules \
-python3-empy \
-python3-setuptools \
-ros-melodic-navigation \
-ros-melodic-teb-local-planner \
-ros-melodic-mpc-local-planner \
+ros-noetic-navigation \
+ros-noetic-teb-local-planner \
+ros-noetic-mpc-local-planner \
 libarmadillo-dev \
-ros-melodic-nlopt \
+ros-noetic-nlopt \
+ros-noetic-geometry2
 
 poetry install
 
-poetry shell
+source $(poetry env info --path)/bin/activate
 
 PYTHONPATH=$(which python)
 
 rosws update
 source $HOME/.bashrc
 cd ../.. 
-catkin_make -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=$PYTHONPATH
+catkin_make -DCMAKE_BUILD_TYPE=Release
 source devel/setup.sh
