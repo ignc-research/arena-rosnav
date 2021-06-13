61774504a5a23b52440a7aef612276a17adea85c
Sirupli <junhuili9408@gmail.com>
2021-06-13 10:32:46 +0800

    add new metrics

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/utils/CSVWriter.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/utils/CSVWriter.py
@@ -6,7 +6,7 @@ class CSVWriter():
         # create evaluation.csv file
         with open('evaluation.csv', 'w+', newline = '') as fp:
             a = csv.writer(fp, delimiter = ',')
-            data = [['episode', 'nearest distance for adult','nearest distance for child','nearest distance for elder','reward','done_reason']]
+            data = [['episode', 'nearest distance for adult','nearest distance for child','nearest distance for elder', 'time DExc for adult', 'time DExc for child', 'time DExc for elder', 'reward', 'done_reason', 'total time steps', 'total time', 'path']]
             a.writerows(data)
 
     def addData(self, data:np.array):
