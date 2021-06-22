cec1359bc8d23a9cd03d6ade1b93790a9f33def7
dangaw255 <danielgawrischgithub@gmail.com>
2021-06-22 19:58:24 +0200

    develop shape drawing

--- gui/FlatlandBodyEditor.py
+++ gui/FlatlandBodyEditor.py
@@ -1,5 +1,136 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
 from FlatlandModel import FlatlandBody, B2BodyType, FlatlandModel
+import random
+
+class ArenaQGraphicsView(QtWidgets.QGraphicsView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setDragMode(QtWidgets.QGraphicsView.DragMode.ScrollHandDrag)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.setSceneRect(-100, -100, 200, 200)
+
+        # add coordinate system lines
+        pen = QtGui.QPen()
+        pen.setWidthF(0.1)
+        self.scene().addLine(0, -100, 0, 100, pen)
+        self.scene().addLine(-100, 0, 100, 0, pen)
+
+        # set initial view
+        rect = QtCore.QRectF(-20, -20, 40, 40)
+        self.fitInView(rect, mode=QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+
+    def wheelEvent(self, event):
+        """
+        Zoom in or out of the view.
+        """
+        zoomInFactor = 1.25
+        zoomOutFactor = 1 / zoomInFactor
+
+        # Save the scene pos
+        oldPos = self.mapToScene(event.pos())
+
+        # Zoom
+        if event.angleDelta().y() > 0:
+            zoomFactor = zoomInFactor
+        else:
+            zoomFactor = zoomOutFactor
+        self.scale(zoomFactor, zoomFactor)
+
+        # Get the new position
+        newPos = self.mapToScene(event.pos())
+
+        # Move scene to old position
+        delta = newPos - oldPos
+        self.translate(delta.x(), delta.y())
+
+
+class FootprintWidget(QtWidgets.QFrame):
+    index = 0
+
+    def __init__(self, polygon: QtWidgets.QGraphicsPolygonItem, scene: QtWidgets.QGraphicsScene, view: QtWidgets.QGraphicsView, **kwargs):
+        super().__init__(**kwargs)
+        self.index = FootprintWidget.index
+        FootprintWidget.index += 1
+        self.points = [[point.x(), point.y()] for point in polygon.polygon()]  # 2D array
+        self.polygon = polygon
+        self.gscene = scene
+        self.gview = view
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.setLayout(QtWidgets.QVBoxLayout())
+        self.setFrameStyle(QtWidgets.QFrame.Shape.Box | QtWidgets.QFrame.Shadow.Raised)
+        self.setSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Maximum)
+
+        # label
+        self.name_label = QtWidgets.QLabel("Polygon " + str(self.index))
+        self.name_label.setFixedHeight(20)
+        self.name_label.setTextFormat(QtCore.Qt.TextFormat.MarkdownText)
+        self.layout().addWidget(self.name_label)
+
+        self.points_frame = QtWidgets.QFrame()
+        self.points_frame.setLayout(QtWidgets.QVBoxLayout())
+        self.layout().addWidget(self.points_frame)
+
+        button = QtWidgets.QPushButton("print points")
+        button.clicked.connect(lambda x: print(self.points))
+        self.layout().addWidget(button)
+
+        for i, point in enumerate(self.points):
+            self.add_point(i, point)
+
+    def add_point(self, id, point=None):
+        # widget
+        widget = QtWidgets.QWidget()
+        widget.setLayout(QtWidgets.QHBoxLayout())
+        widget.setSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Maximum)
+        self.points_frame.layout().addWidget(widget)
+
+        # x value
+        ## label
+        x_label = QtWidgets.QLabel("x")
+        widget.layout().addWidget(x_label)
+        ## spinbox
+        x_spinbox = QtWidgets.QDoubleSpinBox()
+        if point != None:
+            x_spinbox.setValue(point[0])
+        else:
+            x_spinbox.setValue(0.0)
+        x_spinbox.setMinimum(-100.0)
+        x_spinbox.setSingleStep(0.1)
+        x_spinbox.valueChanged.connect(lambda value: self.update_point(value, id, 0))
+        widget.layout().addWidget(x_spinbox)
+
+        # y value
+        ## label
+        y_label = QtWidgets.QLabel("y")
+        widget.layout().addWidget(y_label)
+        ## spinbox
+        y_spinbox = QtWidgets.QDoubleSpinBox()
+        if point != None:
+            y_spinbox.setValue(point[1])
+        else:
+            y_spinbox.setValue(0.0)
+        y_spinbox.setMinimum(-100.0)
+        y_spinbox.setSingleStep(0.1)
+        y_spinbox.valueChanged.connect(lambda value: self.update_point(value, id, 1))
+        widget.layout().addWidget(y_spinbox)
+
+        if point == None:
+            point = [0.0, 0.0]
+            self.points.append(point)
+
+    def update_point(self, value, point_id, point_index):
+        self.points[point_id][point_index] = value
+        self.update_polygon()
+        
+
+    def update_polygon(self):
+        new_points = [QtCore.QPointF(element[0], element[1]) for element in self.points]
+        new_polygon = QtGui.QPolygonF(new_points)
+        self.polygon.setPolygon(new_polygon)
+
 
 class FlatlandBodyEditor(QtWidgets.QWidget):
     def __init__(self, id, model, **kwargs):
@@ -7,41 +138,41 @@ class FlatlandBodyEditor(QtWidgets.QWidget):
         self.id = id
         self.flatland_body = FlatlandBody()
         model.bodies[id] = self.flatland_body
+        self.polygons = []
         self.setup_ui()
     
     def setup_ui(self):
         self.setWindowTitle("Flatland Body Editor")
-
-        self.layout = QtWidgets.QGridLayout()
-        self.setLayout(self.layout)
+        self.setLayout(QtWidgets.QGridLayout())
+        self.resize(1000, 600)
 
         # name
         ## label
         name_label = QtWidgets.QLabel("### Name")
         name_label.setTextFormat(QtCore.Qt.TextFormat.MarkdownText)
-        self.layout.addWidget(name_label, 0, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout().addWidget(name_label, 0, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
         ## editbox
         self.name_edit = QtWidgets.QLineEdit("new_body")
         self.name_edit.setFixedSize(150, 30)
-        self.layout.addWidget(self.name_edit, 0, 1, QtCore.Qt.AlignmentFlag.AlignRight)
+        self.layout().addWidget(self.name_edit, 0, 1, QtCore.Qt.AlignmentFlag.AlignRight)
 
         # type
         ## label
         type_label = QtWidgets.QLabel("### Type")
         type_label.setTextFormat(QtCore.Qt.TextFormat.MarkdownText)
-        self.layout.addWidget(type_label, 1, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout().addWidget(type_label, 1, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
         ## dropdown
         self.type_dropdown = QtWidgets.QComboBox()
         for body_type in B2BodyType:
             self.type_dropdown.insertItem(body_type.value, body_type.name.lower())
         self.type_dropdown.setFixedSize(150, 30)
-        self.layout.addWidget(self.type_dropdown, 1, 1, QtCore.Qt.AlignmentFlag.AlignRight)
+        self.layout().addWidget(self.type_dropdown, 1, 1, QtCore.Qt.AlignmentFlag.AlignRight)
 
         # color
         ## label
         color_label = QtWidgets.QLabel("### Color")
         color_label.setTextFormat(QtCore.Qt.TextFormat.MarkdownText)
-        self.layout.addWidget(color_label, 2, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout().addWidget(color_label, 2, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
         # color dialog
         self.color_dialog = QtWidgets.QColorDialog()
         self.color = self.flatland_body.color
@@ -50,73 +181,96 @@ class FlatlandBodyEditor(QtWidgets.QWidget):
         self.color_button.setFixedSize(150, 30)
         self.color_button.setStyleSheet(f"background-color: {self.flatland_body.color.name()}")
         self.color_button.clicked.connect(self.on_color_button_clicked)
-        self.layout.addWidget(self.color_button, 2, 1, QtCore.Qt.AlignmentFlag.AlignRight)
+        self.layout().addWidget(self.color_button, 2, 1, QtCore.Qt.AlignmentFlag.AlignRight)
 
         # linear damping
         ## label
         linear_damping_label = QtWidgets.QLabel("### Linear Damping")
         linear_damping_label.setTextFormat(QtCore.Qt.TextFormat.MarkdownText)
-        self.layout.addWidget(linear_damping_label, 3, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout().addWidget(linear_damping_label, 3, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
         ## spinbox
         self.linear_damping_spin_box = QtWidgets.QDoubleSpinBox()
         self.linear_damping_spin_box.setValue(0.0)
         self.linear_damping_spin_box.setSingleStep(0.1)
         self.linear_damping_spin_box.setMinimum(0.0)
         self.linear_damping_spin_box.setFixedSize(150, 30)
-        self.layout.addWidget(self.linear_damping_spin_box, 3, 1, QtCore.Qt.AlignmentFlag.AlignRight)
+        self.layout().addWidget(self.linear_damping_spin_box, 3, 1, QtCore.Qt.AlignmentFlag.AlignRight)
 
         # angular damping
         ## label
         angular_damping_label = QtWidgets.QLabel("### Angular Damping")
         angular_damping_label.setTextFormat(QtCore.Qt.TextFormat.MarkdownText)
-        self.layout.addWidget(angular_damping_label, 4, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout().addWidget(angular_damping_label, 4, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
         ## spinbox
         self.angular_damping_spin_box = QtWidgets.QDoubleSpinBox()
         self.angular_damping_spin_box.setValue(0.0)
         self.angular_damping_spin_box.setSingleStep(0.1)
         self.angular_damping_spin_box.setMinimum(0.0)
         self.angular_damping_spin_box.setFixedSize(150, 30)
-        self.layout.addWidget(self.angular_damping_spin_box, 4, 1, QtCore.Qt.AlignmentFlag.AlignRight)
+        self.layout().addWidget(self.angular_damping_spin_box, 4, 1, QtCore.Qt.AlignmentFlag.AlignRight)
 
         # drawing frame
-        # self.drawing_frame = QtWidgets.QFrame()
-        self.scene = QtWidgets.QGraphicsScene()
-        self.scene.addText("hello\n\nworld")
+        ## frame
+        drawing_frame = QtWidgets.QFrame()
+        drawing_frame.setLayout(QtWidgets.QVBoxLayout())
+        drawing_frame.setFrameStyle(QtWidgets.QFrame.Shape.Box | QtWidgets.QFrame.Shadow.Raised)
+        self.layout().addWidget(drawing_frame, 0, 2, 8, 1)
+        ## graphicsscene
+        self.gscene = QtWidgets.QGraphicsScene()
+        ## graphicsview
+        self.gview = ArenaQGraphicsView(self.gscene)
+        drawing_frame.layout().addWidget(self.gview)
 
-        self.myview = QtWidgets.QGraphicsView(self.scene)
-        self.layout.addWidget(self.myview, 5, 0, 1, -1)
-        # self.myview.show()
+        # add polygon button
+        self.add_polygon_button = QtWidgets.QPushButton("Add Polygon")
+        self.add_polygon_button.clicked.connect(self.on_add_polygon_clicked)
+        self.layout().addWidget(self.add_polygon_button, 6, 0, 1, 2)
 
-        # self.drawing_frame = QtWidgets.QGraphicsView()
-        # self.drawing_frame.setMinimumHeight(100)
-        # self.layout.addWidget(self.drawing_frame)
-
-        points = QtGui.QPolygonF(
-            [
-                QtCore.QPointF(10.0, 80.0),
-                QtCore.QPointF(20.0, 10.0),
-                QtCore.QPointF(80.0, 30.0),
-                QtCore.QPointF(90.0, 70.0)
-            ]
-        )
-
-        self.scene.addPolygon(points)
-
-        # painter = QtGui.QPainter(self.drawing_frame)
-        # painter.setRenderHint(QtGui.QPainter.Antialiasing, False)
-        # painter.setPen(self.palette().dark().color())
-        # painter.setBrush(QtCore.Qt.NoBrush)
-        # painter.drawRect(QtCore.QRect(0, 0, self.width() - 1, self.height() - 1))
-
-        # painter.drawPolygon(points, 4)
-
+        # footprints
+        ## scrollarea
+        self.footprints_scrollarea = QtWidgets.QScrollArea(self)
+        self.footprints_scrollarea.setWidgetResizable(True)
+        self.layout().addWidget(self.footprints_scrollarea, 7, 0, 1, 2)
+        ## frame
+        self.footprints_frame = QtWidgets.QFrame()
+        self.footprints_frame.setLayout(QtWidgets.QVBoxLayout())
+        spacer = QtWidgets.QSpacerItem(1, 1, vPolicy=QtWidgets.QSizePolicy.Policy.Expanding)
+        self.footprints_frame.layout().addSpacerItem(spacer)
+        self.footprints_scrollarea.setWidget(self.footprints_frame)
 
         # save button
         self.save_button = QtWidgets.QPushButton("Save and Close")
         self.save_button.clicked.connect(self.on_save_clicked)
-        self.layout.addWidget(self.save_button, 6, 0, 1, -1)
+        self.layout().addWidget(self.save_button, 8, 0, 1, -1)
 
-        self.resize(500, 200)
+
+    def on_add_polygon_clicked(self):
+        # create default polygon
+        polygon = QtGui.QPolygonF(
+            [
+                QtCore.QPointF(0.0, 0.0),
+                QtCore.QPointF(10.0, 0.0),
+                QtCore.QPointF(10.0, 10.0),
+                QtCore.QPointF(0.0, 10.0),
+            ]
+        )
+        # create brush
+        brush = QtGui.QBrush(QtGui.QColor("black"), QtCore.Qt.BrushStyle.SolidPattern)
+        # create pen
+        pen = QtGui.QPen()
+        pen.setWidthF(0.5)
+        pen.setStyle(QtCore.Qt.PenStyle.SolidLine)
+        pen.setCapStyle(QtCore.Qt.PenCapStyle.RoundCap)
+        pen.setJoinStyle(QtCore.Qt.PenJoinStyle.RoundJoin)
+        # add polygon to scene
+        polygon_item = self.gscene.addPolygon(polygon, pen, brush)
+        polygon_item.setFlags(QtWidgets.QGraphicsItem.GraphicsItemFlag.ItemIsMovable | QtWidgets.QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
+        # fit to view
+        # rect = self.gscene.itemsBoundingRect() + QtCore.QMarginsF(-20, -20, 40, 40)
+        # self.gview.fitInView(rect, mode=QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+
+        # add FootprintWidget to list
+        self.footprints_frame.layout().insertWidget(0, FootprintWidget(polygon_item, self.gscene, self.gview))
 
     def on_color_button_clicked(self):
         # get color
@@ -142,17 +296,6 @@ class FlatlandBodyEditor(QtWidgets.QWidget):
         # hide window
         self.hide()
 
-    # def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
-    #     painter = QtGui.QPainter(self.drawing_frame)
-    #     # painter = QtGui.QPainter(self)
-    #     painter.setRenderHint(QtGui.QPainter.Antialiasing, False)
-    #     painter.setPen(self.palette().dark().color())
-    #     painter.setBrush(QtCore.Qt.NoBrush)
-    #     painter.drawRect(QtCore.QRect(0, 0, self.width() - 10, self.height() - 10))
-
-    #     # return super().paintEvent(a0)
-    
-
     def closeEvent(self, event):
         # reset to values already saved
         self.name_edit.setText(self.flatland_body.name)
@@ -166,6 +309,8 @@ if __name__ == "__main__":
 
     model = FlatlandModel()
     widget = FlatlandBodyEditor(0, model)
+    widget.on_add_polygon_clicked()
+    # widget.on_add_polygon_clicked()
     widget.show()
 
     app.exec()
