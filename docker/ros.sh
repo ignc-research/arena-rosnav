37b8cf6638b1bb299a08c762d2f5f01c5a3cee71
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-06 20:52:49 +0200

    Update ros.sh

--- docker/ros.sh
+++ docker/ros.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-trainmode = $1
+taskmode = $1
 map=$2
 n_envs = $3
 
-roslaunch arena_bringup start_arena_flatland.launch train_mode:=true use_viz:=false task_mode:=$trainmode map_file:=$map num_envs:=$n_envs
+roslaunch arena_bringup start_arena_flatland.launch train_mode:=true use_viz:=false task_mode:=$taskmode map_file:=$map num_envs:=$n_envs
