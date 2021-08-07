63c6c108893a45b9c1dc0ec257737bbec6d454ea
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 01:35:03 +0200

    Update py_train.sh

--- docker/py_train.sh
+++ docker/py_train.sh
@@ -1,4 +1,11 @@
  #!/bin/bash
+
 params="$1"
+
+activate () {
+  /root/.python_env/rosnav/bin/activate
+}
+
+activate
 
 echo ${params}|xargs python /root/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py
