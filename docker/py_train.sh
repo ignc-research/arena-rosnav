adcd32355d8009681077a2f0024079e0bdcce684
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 01:51:43 +0200

    Update py_train.sh

--- docker/py_train.sh
+++ docker/py_train.sh
@@ -2,10 +2,14 @@
 
 params="$1"
 
-activate () {
-  /root/.python_env/rosnav/bin/activate
-}
+source /opt/ros/melodic/setup.sh
 
-activate
+export WORKON_HOME=/root/.python_env
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+export PYTHONPATH=/root/catkin_ws/src/arena-rosnav:${PYTHONPATH}
+source /usr/local/bin/virtualenvwrapper.sh
+source /root/catkin_ws/devel/setup.bash
+export PYTHONPATH=//geometry2_ws/devel/lib/python3/dist-packages:${PYTHONPATH}
 
 echo ${params}|xargs python /root/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py
