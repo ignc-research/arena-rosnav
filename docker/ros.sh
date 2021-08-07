0d40fadb9748b4d3d5e15eadc3c096f619b04916
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 01:44:20 +0200

    Update ros.sh

--- docker/ros.sh
+++ docker/ros.sh
@@ -11,12 +11,4 @@ source /usr/local/bin/virtualenvwrapper.sh
 source /root/catkin_ws/devel/setup.bash
 export PYTHONPATH=//geometry2_ws/devel/lib/python3/dist-packages:${PYTHONPATH}
 
-echo python --version
-activate () {
-  /root/.python_env/rosnav/bin/activate
-}
-
-activate
-echo python --version
-
-roslaunch arena_bringup start_arena_flatland.launch "$params"
+roslaunch arena_bringup start_arena_flatland.launch $params
