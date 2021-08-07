9bf63486a4639e63c93f6dd04af794ed9323320d
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 00:06:49 +0200

    Update entrypoint_train.sh

--- docker/entrypoint_train.sh
+++ docker/entrypoint_train.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # roslaunch arena_bringup start_arena_flatland.launch  train_mode:=true 	use_viz:=true  task_mode:=random
-roslaunch=$1
-python_train=$2
+roslaunch="$1"
+python_train="$2"
 
-echo $roslaunch
-echo $python_train
+echo "$roslaunch"
+echo "$python_train"
 
-screen -dmS roslaunch bash -c "source ./ros.sh $roslaunch"
+screen -dmS roslaunch bash -c "source ./ros.sh "$roslaunch""
 screen -S roslaunch -X logfile screenlog_roslaunch.log
 screen -S roslaunch -X log
 sleep 10
@@ -14,7 +14,7 @@ sleep 10
 # python scripts/training/train_agent.py --agent MLP_ARENA2D
 
 #bash ./py_train.sh $trainmode $agent
-screen -dmS python_training bash -c "source ./py_train.sh $python_train"
+screen -dmS python_training bash -c "source ./py_train.sh "$python_train""
 screen -S python_training -X logfile screenlog_python_train.log
 screen -S python_training -X log
 sleep 4
