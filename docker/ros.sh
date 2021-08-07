1b8a96b0afe58a8e886df09f6ed75016f0a6e138
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 01:37:53 +0200

    Update ros.sh

--- docker/ros.sh
+++ docker/ros.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 params="$1"
 echo "$params"
-
+python --verson
 activate () {
   /root/.python_env/rosnav/bin/activate
 }
 
 activate
+python --version
 roslaunch arena_bringup start_arena_flatland.launch "$params"
