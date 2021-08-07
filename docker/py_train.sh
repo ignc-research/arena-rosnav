9e00100d324d2937fe021c8057c33455b7ba74ad
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 00:55:28 +0200

    Update py_train.sh

--- docker/py_train.sh
+++ docker/py_train.sh
@@ -1,9 +1,4 @@
- 
-#!/bin/bash
-#get params
-#MLP_ARENA2D
-#column
-
+ #!/bin/bash
 params="$1"
 
 echo ${params}|xargs python /root/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py
