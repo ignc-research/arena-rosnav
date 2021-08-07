0338d9a8f34bab7bef1a69e4f8322aed436df13a
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 01:58:29 +0200

    Update py_train.sh

--- docker/py_train.sh
+++ docker/py_train.sh
@@ -11,5 +11,6 @@ export PYTHONPATH=/root/catkin_ws/src/arena-rosnav:${PYTHONPATH}
 source /usr/local/bin/virtualenvwrapper.sh
 source /root/catkin_ws/devel/setup.bash
 export PYTHONPATH=//geometry2_ws/devel/lib/python3/dist-packages:${PYTHONPATH}
+source /root/.python_env/rosnav/bin/activate
 
 echo ${params}|xargs python /root/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py
