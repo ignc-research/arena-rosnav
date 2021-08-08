139ff6001805b97f2ead457e86bdeac7ca525e56
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 09:09:29 +0200

    dont need screen for py_train, because after screen start, cointainer will close, doesnt matter if the job in screen is finished or not

--- docker/entrypoint_train.sh
+++ docker/entrypoint_train.sh
@@ -17,24 +17,11 @@ source /root/catkin_ws/devel/setup.bash
 export PYTHONPATH=//geometry2_ws/devel/lib/python3/dist-packages:${PYTHONPATH}
 source /root/.python_env/rosnav/bin/activate
 
-screen -dmS roslaunch bash -c "source ./ros.sh "$roslaunch""
+screen -dmS roslaunch bash -c "source ./docker/ros.sh "$roslaunch""
 screen -S roslaunch -X logfile screenlog_roslaunch.log
 screen -S roslaunch -X log
 sleep 10
-
-# python scripts/training/train_agent.py --agent MLP_ARENA2D
-
-#bash ./py_train.sh $trainmode $agent
-screen -dmS python_training bash -c "echo "python --version" && source ./py_train.sh "$python_train""
-screen -S python_training -X logfile screenlog_python_train.log
-screen -S python_training -X log
-sleep 4
-
-screen -dmS python_test python /root/catkin_ws/src/arena-rosnav/arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/scripts/training/train_agent.py "$python_train"
-screen -S python_test -X logfile screenlog_python_test.log
-screen -S python_test -X log
-sleep 4
-
-
 screen -ls
-# bash
+
+# python scripts/training/train_agent.py
+bash ./docker/py_train.sh "$python_train"
