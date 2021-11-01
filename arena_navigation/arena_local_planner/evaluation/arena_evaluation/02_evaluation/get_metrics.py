8ae2834728148768d9d38314dd168d7addd90aab
Duc <duc.pichel@gmail.com>
2021-11-01 10:59:14 +0100

    update get_metrics

--- arena_navigation/arena_local_planner/evaluation/arena_evaluation/02_evaluation/get_metrics.py
+++ arena_navigation/arena_local_planner/evaluation/arena_evaluation/02_evaluation/get_metrics.py
@@ -4,39 +4,44 @@ import glob # usefull for listing all files of a type in a directory
 import os
 import time
 import yaml
+import json
+import matplotlib.pyplot as plt
 
 class get_metrics():
-    def __init__(self) -> None:
+    def __init__(self):
         self.dir_path = os.path.dirname(os.path.abspath(__file__)) # get path for current file, does not work if os.chdir() was used
         self.data_dir = os.path.dirname(self.dir_path) + "/01_recording" # parent_directory_path + directory name where csv files are located
+        self.now = time.strftime("%y-%m-%d_%H:%M:%S")
         self.read_config()
 
     def read_config(self):
         with open(self.dir_path+"/get_metrics_config.yaml") as file:
             self.config = yaml.safe_load(file)
 
-    def grab_data(self):
-        files = glob.glob("{0}/*.csv".format(self.data_dir)) # get all the csv files paths in the directory /01_recording
-        for file in files:
-            file_name = file.split("/")[-1]
-            os.rename(self.data_dir+"/"+file_name, self.dir_path+"/"+file_name) # move file from data_dir to dir_path (where this script is located)
-
     def evaluate_data(self): # read in all csv files and compute metrics
         data = {}
-        files = glob.glob("{0}/*.csv".format(self.dir_path)) # get all the csv files paths in the directory where this script is located
+        files = glob.glob("{0}/*.csv".format(self.data_dir)) # get all the csv files paths in the directory where this script is located
         for file in files: # summarize all the csv files and add to dictionary
-            df = pd.read_csv(file, converters = {"laser_scan":self.string_to_float_list, "action": self.string_to_float_list})
+            df = self.extend_df(pd.read_csv(file, converters = {"laser_scan":self.string_to_float_list, "action": self.string_to_float_list}))
             file_name = file.split("/")[-1].split("_")[:-2] # cut off date and time and .csv ending
             file_name = "_".join(file_name) # join together to only include local planner, map and obstacle number
             data[file_name] = {
-                "df": self.extend_df(df),
-                "summary_df": [],
-                "paths_travelled": []
+                "df": df.to_dict(orient = "list"),
+                "summary_df": self.get_summary_df(df),
+                "paths_travelled": self.get_paths_travelled(df)
             }
-            return data
+        # self.grab_data(files)
+        # with open(self.dir_path+"/data_{}.json".format(self.now), "w") as outfile:
+        #     json.dump(data, outfile)
 
+    def grab_data(self,files): # move data from 01_recording into 02_evaluattion into a data folder with timestamp
+        os.mkdir(self.dir_path+"/data_{}".format(self.now))
+        for file in files:
+            file_name = file.split("/")[-1]
+            os.rename(self.data_dir+"/"+file_name, self.dir_path+"/data_{}/".format(self.now)+file_name) # move file from dir_path to data folder
+    
     def string_to_float_list(self,df_column): # convert list from csv saved as string to list of floats
-        return np.array((df_column.replace("[","").replace("]","").split(", "))).astype(float)
+        return list(np.array((df_column.replace("[","").replace("]","").split(", "))).astype(float))
 
     def extend_df(self,df):
         df["collision"] = [np.any(np.less_equal(x,self.config["robot_radius"])) for x in df["laser_scan"]]
@@ -46,6 +51,7 @@ class get_metrics():
         df["min_clearing_distance"] = [np.nanmin(x) for x in df["laser_scan"]]
         df["mean_clearing_distance"] = [np.nanmean(x) for x in df["laser_scan"]]
         df["median_clearing_distance"] = [np.nanmedian(x) for x in df["laser_scan"]]
+        return df
 
     def get_action_type(self,df):
         action_type_column = []
@@ -67,7 +73,67 @@ class get_metrics():
                 computation_time_column.append(x-df["time"][i-1])
         return computation_time_column
 
+    def get_summary_df(self,df): # NOTE: column specification hardcoded !
+        sum_df = df.groupby(["episode"]).sum()
+        mean_df = df.groupby(["episode"]).mean()
+        summary_df = mean_df
+        summary_df["time"] = self.get_time(df)
+        summary_df["collision"] = sum_df["collision"]
+        summary_df["path_length"] = self.get_path_length(df)
+        summary_df["success"],summary_df["done_reason"]  = self.get_success(summary_df)
+        summary_df = summary_df.drop(columns = ['robot_lin_vel_x', 'robot_lin_vel_y', 'robot_ang_vel', 'robot_orientation', 'robot_pos_x', 'robot_pos_y'])
+        print(summary_df)
+        return summary_df
+    
+    def get_time(self,df):
+        time_list = []
+        episodes = np.unique(df["episode"])
+        for episode in episodes:
+            times = list(df.loc[df["episode"]==episode,"time"])
+            time_list.append(times[-1]-times[0])
+        return time_list
+
+    def get_path_length(self,df):
+        path_length_list = []
+        episodes = np.unique(df["episode"])
+        for episode in episodes:
+            path_length = 0
+            points = list(zip(df.loc[df["episode"]==episode,"robot_pos_x"],df.loc[df["episode"]==episode,"robot_pos_y"]))
+            for i,point in enumerate(points):
+                if i == 0:
+                    continue
+                else:
+                    path_length = path_length + np.linalg.norm(np.array(point)-np.array(points[i-1]))
+            path_length_list.append(path_length)
+        return path_length_list
+
+    def get_success(self,summary_df):
+        success = []
+        done_reason = []
+        # summary_df.loc[3,"collision"] = 45
+        summary_df.loc[3,"time"] = 192
+        for episode in summary_df.index:
+            if summary_df.loc[episode,"collision"] > self.config["collision_treshold"]:
+                success.append(False)
+                done_reason.append("collision")
+            elif summary_df.loc[episode,"time"] > self.config["time_out_treshold"]:
+                success.append(False)
+                done_reason.append("time_out")
+            else:
+                success.append(True)
+                done_reason.append("goal_reached")
+        return success, done_reason
+
+    def get_paths_travelled(self,df):
+        paths_travelled = {}
+        episodes = np.unique(df["episode"])
+        for episode in episodes:
+            paths_travelled[episode] = list(zip(df.loc[df["episode"]==episode,"robot_pos_x"],df.loc[df["episode"]==episode,"robot_pos_y"]))
+            # plt.figure(episode)
+            # plt.scatter(*zip(*paths_travelled[episode]))
+            # plt.show()
+        return paths_travelled
+
 if __name__=="__main__":
     metrics = get_metrics()
-    metrics.grab_data()
     metrics_data = metrics.evaluate_data()
