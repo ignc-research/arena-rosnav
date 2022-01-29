6595623e5326718474c59147aac2b12b2ff48b48
tuananhroman <anh.maxiking@hotmail.de>
2022-01-29 13:24:22 +0100

    dynamic agent loading (observation space extension) for evaluation

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/base_agent_wrapper.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/base_agent_wrapper.py
@@ -117,6 +117,10 @@ class BaseDRLAgent(ABC):
 
         self._agent_params = hyperparams
         self._get_robot_name_from_params()
+        rospy.set_param(
+            "action_in_obs",
+            self._agent_params.get("actions_in_observationspace", False),
+        )
 
     def read_setting_files(
         self, robot_setting_yaml: str, action_space_yaml: str
