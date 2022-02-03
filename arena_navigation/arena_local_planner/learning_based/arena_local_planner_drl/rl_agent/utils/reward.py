9539287aabe89af16f72177bcd8038e696f0fa72
tuananhroman <anh.maxiking@hotmail.de>
2022-02-03 19:35:20 +0100

    removed deprecated history in flatlandenv, tuned rule_05

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/utils/reward.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/utils/reward.py
@@ -206,10 +206,10 @@ class RewardCalculator:
                 reward_factor=0.2,
                 penalty_factor=0.3,
             )
-            self._reward_abrupt_vel_change(vel_idx=0, factor=0.1)
-            self._reward_abrupt_vel_change(vel_idx=-1, factor=0.075)
+            self._reward_abrupt_vel_change(vel_idx=0, factor=0.01)
+            self._reward_abrupt_vel_change(vel_idx=-1, factor=0.0075)
             if self.holonomic:
-                self._reward_abrupt_vel_change(vel_idx=1, factor=0.1)
+                self._reward_abrupt_vel_change(vel_idx=1, factor=0.01)
             self._reward_reverse_drive(self._curr_action, 0.000001)
         else:
             self.last_dist_to_path = None
