c36f56d12108a9d38770c87a6b114209fba00368
flameryx <r.sosamelo@gmail.com>
2022-07-27 16:36:28 +0200

    Added new scripts to record data of outdoor maps

--- arena_bringup/project_team_evals/bo_evals.sh
+++ arena_bringup/project_team_evals/bo_evals.sh
@@ -1,6 +1,6 @@
 local_planners=("dwa")
 robot_models=("burger")
-project_maps=project_maps=($(ls ../../simulator_setup/maps | grep -m 75 "map" | tail -n 25))
+project_maps=project_maps=($(ls ../../simulator_setup/maps | grep -m 75 "map1[0-9][0-9]" | tail -n 25))
 num_dynamic_obs=("5")
 
 num_static_obs="0"
@@ -18,4 +18,4 @@ do for num_dyn in "${num_dynamic_obs[@]}"
 do roslaunch arena_bringup start_arena_flatland.launch model:="$robot" num_dynamic_obs:="$num_dyn" num_static_obs:=$num_static_obs min_dyn_vel:=$min_dyn_vel max_dyn_vel:=$max_dyn_vel min_dyn_radius:=$min_dyn_radius max_dyn_radius:=$max_dyn_radius min_static_radius:=$min_static_radius max_static_radius:=$max_static_radius local_planner:="$planner" map_file:="$map" task_mode:="project_eval" scenario_file:="project_eval/scenario_1.json" use_recorder:="true" show_rviz:="false" use_rviz:="false"; 
 done; done; done; done
 
-python3 createAverage.py ../../../forks/arena-evaluation/01_recording/project_recordings/
+python3 ../../../forks/arena-evaluation/01_recording/createAverageScript/createAverage.py
